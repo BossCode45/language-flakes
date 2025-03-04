e45556cfa3eacf0303254160cd527c51d70d9d05
BossCode45 <human.cyborg42@gmail.com>
2025-03-04 18:08:03 +1300

    First commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    description = "Flakes for different languages";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    outputs = { self, nixpkgs, ... }@inputs:
+        inputs.flake-utils.lib.eachDefaultSystem (system: let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            devShells = {
+                java = pkgs.mkShell {
+                    nativeBuildInputs = [
+                        pkgs.jdk21
+                        pkgs.jdt-language-server
+                    ];
+                };
+                cpp = pkgs.mkShell {
+                  nativeBuildInputs = [
+                      pkgs.gcc
+                      pkgs.gnumake
+                      pkgs.clang-tools
+                  ];
+                };
+            };
+        });
+}
